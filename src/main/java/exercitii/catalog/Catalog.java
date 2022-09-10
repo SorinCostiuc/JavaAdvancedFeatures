45e9c2f5d0b1bf8a3680226f7b6c3fa4ec12896b
Sorin Costiuc <sorin.costiuc90@gmail.com>
2022-09-10 10:30:23 +0300

    Finish recap

--- src/main/java/exercitii/catalog/Catalog.java
+++ src/main/java/exercitii/catalog/Catalog.java
@@ -3,27 +3,27 @@ package exercitii.catalog;
 import java.util.*;
 
 public class Catalog {
-    Map<Student, Map<Course, List<Grade>>> studentGrades;
+    Map<Student, Map<Course, List<Grade>>> gradesBook;
 
-    public Catalog(Map<Student, Map<Course, List<Grade>>> studentGrades) {
-        this.studentGrades = studentGrades;
+    public Catalog(Map<Student, Map<Course, List<Grade>>> gradesBook) {
+        this.gradesBook = gradesBook;
     }
 
     public Catalog() {
-        this.studentGrades = new HashMap<>();
+        this.gradesBook = new HashMap<>();
     }
 
 
     public Catalog addGrade(Student student, Grade grade) {
-        if (studentGrades.get(student) == null) {
-            studentGrades.put(student, new HashMap<>() {{
-                put(grade.getCourse(), new ArrayList<>(Arrays.asList(grade)));
+        if (gradesBook.get(student) == null) {
+            gradesBook.put(student, new HashMap<>() {{
+                put(grade.getCourse(), new ArrayList<>(List.of(grade)));
             }});
         } else {
-            if (studentGrades.get(student).get(grade.getCourse()) == null) {
-                studentGrades.get(student).put(grade.getCourse(), new ArrayList<>(Arrays.asList(grade)));
+            if (gradesBook.get(student).get(grade.getCourse()) == null) {
+                gradesBook.get(student).put(grade.getCourse(), new ArrayList<>(List.of(grade)));
             } else {
-                studentGrades.get(student).get(grade.getCourse()).add(grade);
+                gradesBook.get(student).get(grade.getCourse()).add(grade);
             }
         }
         return this;
@@ -32,9 +32,9 @@ public class Catalog {
 
     public void printGrades(Student student) {
         System.out.println("\n" + student);
-        Map<Course, List<Grade>> studentGrade = studentGrades.get(student);
-        for (Course course : studentGrade.keySet()) {
-            List<Grade> grades = studentGrade.get(course);
+        Map<Course, List<Grade>> courseGrades = gradesBook.get(student);
+        for (Course course : courseGrades.keySet()) {
+            List<Grade> grades = courseGrades.get(course);
             System.out.println(course);
             for (int i = 0; i < grades.size(); i++) {
                 System.out.print(grades.get(i).getValue() + " ");
@@ -45,9 +45,9 @@ public class Catalog {
 
     public void printAverage(Student student) {
         System.out.println("\n" + student);
-        Map<Course, List<Grade>> studentGrade = studentGrades.get(student);
-        for (Course course : studentGrade.keySet()) {
-            List<Grade> grades = studentGrade.get(course);
+        Map<Course, List<Grade>> courseGrades = gradesBook.get(student);
+        for (Course course : courseGrades.keySet()) {
+            List<Grade> grades = courseGrades.get(course);
             System.out.println(course);
             double sum = 0;
             for (int i = 0; i < grades.size(); i++) {
